27ce47c4c4b25c73970321828787ebbf385f38e8
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-08-07 03:22:04 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,9 +3,12 @@ require 'pry'
 
 def count_elements(array)
   new_hash = {}
+  counter = 0
   
-  array.collect do |a, b|
-    binding.pry
+  array.collect do |animal|
+    #binding.pry
+      if 
+      
   end
   new_hash
 end
